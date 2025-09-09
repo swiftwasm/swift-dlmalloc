890c74f17ca39f91ce36a61429439dbeadcbdb6f
Yuta Saito <kateinoigakukun@gmail.com>
2025-09-09 06:12:53 +0000

    Add BULK_MEMORY_THRESHOLD definition to dlmalloc target
    Allow building with bulk-memory feature enabled.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,9 @@ let package = Package(
             name: "dlmalloc",
             exclude: ["wasm/"],
             cSettings: [
-                .headerSearchPath("wasm/internal-headers")
+                .headerSearchPath("wasm/internal-headers"),
+                // NOTE: Keep the value in sync with https://github.com/WebAssembly/wasi-libc/blob/wasi-sdk-27/Makefile#L44
+                .define("BULK_MEMORY_THRESHOLD", to: "32"),
             ]
         ),
     ]
